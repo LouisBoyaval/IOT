3743daeeb56f069695ffa9f11ef896c93219a1b8
LouisBoyaval <58878936+LouisBoyaval@users.noreply.github.com>
2022-06-06 23:52:39 +0200

    Update Motor.c

--- src/Motor.c
+++ src/Motor.c
@@ -1,13 +1,37 @@
+#include <wiringPi.h>
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
+#include <stdlib.h>
+
+int main (void)
+{
+  if (wiringPiSetup() == -1)
+    exit (1) ;
+
+  pinMode(1,PWM_OUTPUT);
+  pwmSetMode(PWM_MODE_MS);
+  pwmSetClock(1920);
+  pwmSetRange(200);
+  
+  // SI TOURNE DANS LE MAUVAIS SENS, METTRE (1,10)
+  pwmWrite(1,20); //tourne dans un sens
+}
+
+
+
+
+FICHIER STOP.C
 
 #include <wiringPi.h>
-#include <softServo.h>
-#include "../header/DB_connector.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main ()
+int main (void)
 {
-    int idAnimal;
-    DB_sendQuery("SELECT quantite FROM Repas WHERE idAnimal =");
+  printf ("Raspberry Pi wiringPi test program\n") ;
+
+  if (wiringPiSetup() == -1)
+    exit (1) ;
+
+  pinMode(1,PWM_OUTPUT);
+  pwmWrite(1,0); //stop
 }
